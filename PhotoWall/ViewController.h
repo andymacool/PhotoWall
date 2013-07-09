358e5bcb28684d3dfd19195538de58c281a90819
Andy <andy@upthere.com>
2013-07-08 18:03:55 -0700

    Initial Commit

---
+++ PhotoWall/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PhotoWall
+//
+//  Created by Andy Wang on 7/8/13.
+//  Copyright (c) 2013 Andy Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ PhotoWall/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PhotoWall
+//
+//  Created by Andy Wang on 7/8/13.
+//  Copyright (c) 2013 Andy Wang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
